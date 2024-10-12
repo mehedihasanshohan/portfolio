696027198bd3d0451b5f92e368487f0cd3535126
mehedi hasan <mhshohan04@gmail.com>
2024-10-12 17:34:44 +0600

    scroll animation done

--- script.js
+++ script.js
@@ -90,3 +90,27 @@ const header = document.querySelector("header");
 window.addEventListener("scroll",function(){
   header.classList.toggle("sticky", this.window.scrollY > 50)
 })
+
+
+
+
+// parallex
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add("show-items");
+    }
+    else{
+      entry.target.classList.remove("show-items");
+    }
+  });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
